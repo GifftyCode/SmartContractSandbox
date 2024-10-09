03fe5ef3bbdf047c1c1acb07651b91e76445855e
GifftyCode <ulokangozi@yahoo.com>
2024-10-09 10:06:06 +0100

    contributing fxn

--- CrowdFunding/contracts/CrowdFunding.sol
+++ CrowdFunding/contracts/CrowdFunding.sol
@@ -13,5 +13,43 @@ contract CrowdFunding {
     bool isFundingCompleted;
     address public contributor;
 
+
+    event TargetMet(uint256 totalContribution);
+    event TransferSuccessful(address contributor, uint amount);
+    event FundingCompleted(uint deadline);
+
+    modifier onlyOwner{
+        require(msg.sender == owner, 'You are not the owner');
+        _;
+    }
+
+    constructor(uint _target, uint _deadline) {
+        msg.sender == owner;
+        target == _target * 1 ether;
+        deadline ==  block.timestamp + _deadline * 1 minutes;
+        isTargetMet == false;
+        isFundingCompleted == false;
+    }
+
+
+        function contribute() public payable {
+        require(block.timestamp < deadline, 'Funding period not active yet');
+        require(isTargetMet, 'Target has been reached yet');
+        require(isFundingCompleted, 'Funding is not completed yet');
+
+        uint contribution = msg.value;
+        individualContribution[msg.sender] += contribution;
+        totalContribution += contribution;
+
+        if(totalContribution >= target) {
+            isTargetMet = true;
+            emit TargetMet(totalContribution);
+        }
+
+        emit  TransferSuccessful(contributor, contribution);
+
+
+    }
+
 }
 
