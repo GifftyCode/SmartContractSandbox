2fbb40ffbca52d894d2f34437bd832537da88ce8
GifftyCode <ulokangozi@yahoo.com>
2024-10-09 13:30:14 +0100

    updated code

--- CrowdFunding/contracts/CrowdFunding.sol
+++ CrowdFunding/contracts/CrowdFunding.sol
@@ -4,40 +4,39 @@ pragma solidity ^0.8.0;
 
 contract CrowdFunding {
     address owner;
-    uint256 amountContributed;
     uint256 public target;
     uint256  public totalContribution;
     mapping (address => uint) public individualContribution;
-    uint public deadline;
-    bool isTargetMet;
-    bool isFundingCompleted;
-    address public contributor;
+    uint256 public deadline;
+    bool public  isTargetMet;
+    bool public isFundingCompleted;
+   
 
 
     event TargetMet(uint256 totalContribution);
     event TransferSuccessful(address contributor, uint amount);
-    event FundingCompleted(uint deadline);
+    event FundingCompleted(uint totalContribution);
+
+    constructor(uint256 _target, uint256 _deadline) {
+        owner = msg.sender;
+        target = _target * 1 ether;
+        deadline =  block.timestamp + _deadline * 1 minutes;
+        isTargetMet = false;
+        isFundingCompleted = false;
+    }
+
 
     modifier onlyOwner{
         require(msg.sender == owner, 'You are not the owner');
         _;
     }
 
-    constructor(uint _target, uint _deadline) {
-        msg.sender == owner;
-        target == _target * 1 ether;
-        deadline ==  block.timestamp + _deadline * 1 minutes;
-        isTargetMet == false;
-        isFundingCompleted == false;
-    }
-
-
-        function contribute() public payable {
+    function contribute() public payable {
         require(block.timestamp < deadline, 'Funding period not active yet');
-        require(isTargetMet, 'Target has been reached yet');
-        require(isFundingCompleted, 'Funding is not completed yet');
+        require(!isTargetMet, 'Target has ended');
+        require(!isFundingCompleted, 'Funding is already completed');
 
-        uint contribution = msg.value;
+        uint256 contribution = msg.value;
         individualContribution[msg.sender] += contribution;
         totalContribution += contribution;
 
@@ -46,35 +45,41 @@ contract CrowdFunding {
             emit TargetMet(totalContribution);
         }
 
-        emit  TransferSuccessful(contributor, contribution);
+        emit  TransferSuccessful(msg.sender, contribution);
+    }
+
+    function withdrawFunds() public onlyOwner  {
+        require(isTargetMet, 'Target is not yet Met');
+        require(!isFundingCompleted, 'Funding is already');
+
+        isFundingCompleted = true;
+        payable(owner).transfer(address(this).balance);
+
+    }
+
+    function getRefund() public   {
+        require(block.timestamp >= deadline, 'Funding period has not ended yet');
+        require(!isTargetMet, 'Target has been met already');
+        require(individualContribution[msg.sender] > 0, 'No contribution to refund');
+
+        uint256 contribution = individualContribution[msg.sender];
+        individualContribution[msg.sender] = 0;
+        totalContribution -= contribution;
+
+        payable(msg.sender).transfer(contribution);
+
+        emit TransferSuccessful(msg.sender, contribution);
 
 
     }
 
-        function withdraw() external onlyOwner payable {
-            require(isTargetMet, 'Target is not yet Met');
-            require(isFundingCompleted, 'Funding deadline not reached yet');
+    function getCurrentBalance() public view returns(uint) {
+        return address(this).balance;
+    }
 
-            isFundingCompleted = true;
-            payable(owner).transfer(address(this).balance);
-
-        }
-
-            function getRefund() public payable  {
-            require(block.timestamp >= deadline, 'Funding has not ended yet');
-            require(!isTargetMet, 'Target has been met already');
-            require(individualContribution[msg.sender] > 0, 'No contribution to refund');
-
-            uint contribution = individualContribution[msg.sender];
-            individualContribution[msg.sender] = 0;
-            totalContribution -= contribution;
-
-            payable(msg.sender).transfer(contribution);
-
-            emit TransferSuccessful(msg.sender, contribution);
-
-
-        }
+    function extendDeadline(uint _deadline) public onlyOwner {
+        deadline += _deadline * 1;
+    }
 
 }
 
