91f21d037404e81e11ad799d925d09d4065f3263
Gifftybabe <ulokangozi@gmail.com>
2024-10-29 00:43:49 +0100

    Twitter contract

---
+++ contracts/Twitter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// 1️⃣ Create a Twitter Contract 
+// 2️⃣ Create a mapping between user and tweet 
+// 3️⃣ Add function to create a tweet and save it in mapping
+// 4️⃣ Create a function to get Tweet 
+// 5️⃣ Add array of tweets 
+
+contract Twitter {
+    mapping(address => string) internal Tweets;
+
+    function creatTweet(string memory _tweet) external  {
+        Tweets[msg.sender] = _tweet;
+    }
+
+    function getTweet(address _owner) external view returns (string memory _tweet) {
+        return Tweets[_owner];
+    }
+
+}
