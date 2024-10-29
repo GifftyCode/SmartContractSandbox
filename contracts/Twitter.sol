e18a6f56a2274c3dcbb718ecde5684ffcd4daf05
Gifftybabe <ulokangozi@gmail.com>
2024-10-30 00:42:39 +0100

    Twitter code updated

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -6,17 +6,22 @@ pragma solidity ^0.8.0;
 // 2️⃣ Create a mapping between user and tweet 
 // 3️⃣ Add function to create a tweet and save it in mapping
 // 4️⃣ Create a function to get Tweet 
-// 5️⃣ Add array of tweets 
+// 5️⃣ Add array of tweets
 
 contract Twitter {
-    mapping(address => string) internal Tweets;
+    // mapping(address => string) internal Tweets;
+    mapping (address => string[]) internal Tweets;
 
     function creatTweet(string memory _tweet) external  {
-        Tweets[msg.sender] = _tweet;
+        // Tweets[msg.sender] = _tweet;
+        Tweets[msg.sender].push(_tweet);
     }
 
-    function getTweet(address _owner) external view returns (string memory _tweet) {
+    function getTweet(address _owner, uint _i) external view returns (string memory _tweet) {
+        return Tweets[_owner][_i];
+    }
+
+    function getTweets(address _owner) external view returns (string[] memory _tweet) {
         return Tweets[_owner];
     }
-
 }
