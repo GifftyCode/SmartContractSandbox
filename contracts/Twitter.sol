376c4e67ae7f960967d1d5189077f5c2279125f2
Gifftybabe <ulokangozi@gmail.com>
2024-10-31 14:18:46 +0100

    Struct update

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -1,27 +1,31 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-// 1️⃣ Create a Twitter Contract 
-// 2️⃣ Create a mapping between user and tweet 
-// 3️⃣ Add function to create a tweet and save it in mapping
-// 4️⃣ Create a function to get Tweet 
-// 5️⃣ Add array of tweets
-
-contract Twitter {
-    // mapping(address => string) internal Tweets;
-    mapping (address => string[]) internal Tweets;
-
-    function creatTweet(string memory _tweet) external  {
-        // Tweets[msg.sender] = _tweet;
-        Tweets[msg.sender].push(_tweet);
+contract Twiiter {
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
     }
 
-    function getTweet(address _owner, uint _i) external view returns (string memory _tweet) {
-        return Tweets[_owner][_i];
+    mapping(address => Tweet[]) internal tweets;
+
+    function createTweet(string memory _tweet) external {
+        Tweet memory newTweet = Tweet ({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+        tweets[msg.sender].push(newTweet);
+    }
+    
+    function getTweet(address _owner, uint _i) public view returns (Tweet memory) {
+        return tweets[_owner][_i];
     }
 
-    function getTweets(address _owner) external view returns (string[] memory _tweet) {
-        return Tweets[_owner];
-    }
+    function getAllTweet(address _owner) external view returns (Tweet[] memory) {
+        return tweets[_owner];
+    } 
 }
