17788c13dd37cbb956cf5100fe8f9cfd2bf0f276
Gifftybabe <ulokangozi@gmail.com>
2024-11-02 08:29:08 +0100

    likes and unike logic

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -3,13 +3,14 @@ pragma solidity ^0.8.0;
 
 contract Twiiter {
     struct Tweet {
+        uint256 id;
         address author;
         string content;
         uint256 timestamp;
         uint256 likes;
     }
 
-    uint16  MAX_TWEET_LENGTH = 280;
+    uint16 public MAX_TWEET_LENGTH = 280;
     address public owner;
 
     mapping(address => Tweet[]) internal tweets;
@@ -31,13 +32,31 @@ contract Twiiter {
             author: msg.sender,
             content: _tweet,
             timestamp: block.timestamp,
-            likes: 0
+            likes: 0,
+            id: tweets[msg.sender].length
+
         });
         tweets[msg.sender].push(newTweet);
     }
 
     function changeTweetLength(uint16 newTweetLenght) public onlyOwner{
         MAX_TWEET_LENGTH = newTweetLenght;
+    }
+
+    function likeTweet(address _author, uint256 _id) external {
+
+        require(tweets[_author][_id].id == _id, 'Tweet does not exist');
+
+        tweets[_author][_id].likes ++;
+        
+    }
+
+    function unlikeTweet(address _author, uint256 _id) external  {
+
+        require(tweets[_author][_id].id == _id, 'Tweet does not exist');
+        require(tweets[_author][_id].likes > 0, 'Tweet does not have a like');
+
+        tweets[_author][_id].likes --;
     }
     
     function getTweet(uint _i) public view returns (Tweet memory) {
